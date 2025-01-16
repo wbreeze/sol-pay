e58bf34155c111c006168464736fa9d586629a1e
Douglas Lovell <doug@wbreeze.com>
2025-01-16 17:31:34 -0300

    Add dummy list of API routes

---
+++ app/api/route.ts
@@ -0,0 +1,8 @@
+export async function GET(request: Request) {
+  return Response.json([
+      { methods: [ 'get' ], path: '/api/' },
+      { methods: [ 'get' ], path: '/api/tx/' },
+      { methods: [ 'post' ], path: '/api/tx/' },
+  ]);
+}
+

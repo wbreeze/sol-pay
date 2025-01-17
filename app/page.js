bcc7d893a6cd3167282c4918aed5ed33a8eed4b6
Douglas Lovell <doug@wbreeze.com>
2025-01-17 10:13:51 -0300

    Fetch routes for home page

--- app/page.js
+++ app/page.js
@@ -1,11 +1,34 @@
+'use client';
+
 import styles from "./page.module.css";
+import { useState, useEffect } from 'react';
 
 export default function Home() {
+  const url = new URL('/api/', location);
+  const [ routes, setRoutes ] = useState([]);
+
+  useEffect(() => {
+    async function fetchRoutes() {
+      const data = await fetch(url);
+      const fetched = await data.json();
+      setRoutes(fetched);
+    }
+    fetchRoutes();
+  }, []);
+
   return (
     <div className={styles.page}>
       <main className={styles.main}>
-        Hello Next.js World
+        <ul className='api-routes'>
+          {routes.map((route, index) => (
+            <li key={index}>
+              {route.methods.join(", ").toUpperCase()}: <a
+                href={route.path}>{route.path}</a>
+            </li>
+          ))}
+        </ul>
       </main>
     </div>
   );
 }
+

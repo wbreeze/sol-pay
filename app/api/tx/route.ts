9e1e71a9763ffe5dac719de38e34a7b860181db6
Douglas Lovell <doug@wbreeze.com>
2025-01-17 17:53:15 -0300

    Recode transfer transaction in web3 v2

--- app/api/tx/route.ts
+++ app/api/tx/route.ts
@@ -1,8 +1,13 @@
 import {
-  //clusterApiUrl, Cluster, Connection, Keypair, PublicKey,
-  //LAMPORTS_PER_SOL, SystemProgram, Transaction,
-  Address
-} from "@solana/web3.js"
+  Address, address, generateKeyPair, getAddressFromPublicKey,
+  createSolanaRpc, getLatestBlockhash,
+  createTransactionMessage, setTransactionMessageFeePayer,
+  setTransactionMessageLifetimeUsingBlockhash,
+  appendTransactionMessageInstructions,
+  getBase64EncodedWireTransaction,
+  getTransactionEncoder
+} from "@solana/web3.js";
+import { getTransferSolInstruction } from '@solana-program/system';
 
 export async function GET(req: Request) {
   try {
@@ -18,49 +23,58 @@ export async function GET(req: Request) {
   }
 }
 
-export async function POST(req: Request) {
+export async function POST(req: NextRequest) {
   try {
-    const recipientAcct = "AcctPlaceholder";
-//    const recipientAcct = process.env.TX_RECIPIENT_KEY ||
-//      Keypair.generate().publicKey;
-//    const networkId = (process.env.TX_NETWORK || 'devnet') as Cluster;
+    const DEVNET_RPC = "https://api.devnet.solana.com"
+
+    const data = await req.json();
+    console.log("POST Recieved", data);
+
+    const toAccount = process.env.TX_RECIPIENT_ACCOUNT;
+    console.log("Transfer to", toAccount);
+
+    const payerAccountStr = data.payerAccount;
+    console.log("Payer occount", payerAccountStr);
+    const referenceStr = data.reference;
+    console.log("Reference address", referenceStr);
+
+    const recipientAccount = address(toAccount);
+    const payerAccount = address(payerAccountStr);
+    const reference = address(referenceStr);
+
     const lamportCount = Number(process.env.TX_LAMPORTS) || 1;
-//
-//    const { payerAccount, reference } = req.body;
-//    const payerAccountPK = new PublicKey(payerAccount);
-//    const referencePK = new PublicKey(reference);
-//    const connection = new Connection(clusterApiUrl(networkId));
-//    const { blockhash } = await connection.getLatestBlockhash();
-//
-//    const transaction = new Transaction({
-//      recentBlockhash: blockhash,
-//      feePayer: payerAccountPK,
-//    });
-//
-//    const instruction = SystemProgram.transfer({
-//      fromPubkey: new PublicKey(payerAccount),
-//      toPubkey: new PublicKey(recipientAcct),
-//      lamports: lamportCount,
-//    });
-//
-//    instruction.keys.push({
-//      pubkey: referencePK,
-//      isSigner: false,
-//      isWritable: false,
-//    });
-//
-//    transaction.add(instruction);
-//
-//    const serializedTransaction = transaction.serialize({
-//      requireAllSignatures: false,
-//    });
-//
-//    const base64TxAddr = serializedTransaction.toString("base64");
+    console.log("Lamports", lamportCount);
+
+    const instruction = getTransferSolInstruction({
+      source: payerAccount,
+      destination: recipientAccount,
+      amount: lamportCount
+    });
+
+    const rpcEndpoint = process.env.TX_RPC_ENDPOINT || DEVNET_RPC;
+    const rpc = createSolanaRpc(rpcEndpoint);
+    const latestBlock = await rpc.getLatestBlockhash().send();
+    const latestBlockhash = latestBlock.value.blockhash;
+    console.log("Latest hash", latestBlockhash);
+
+    let tx = createTransactionMessage({ version: 0 });
+    tx = setTransactionMessageFeePayer(payerAccount, tx);
+    tx = setTransactionMessageLifetimeUsingBlockhash(latestBlockhash, tx);
+    tx = appendTransactionMessageInstructions([instruction], tx);
+
+    console.log("Transaction", JSON.stringify(tx, null, 2));
+
+    const tec = getTransactionEncoder();
+    console.log("Encoder", tec);
+    const serializedTransaction = tec.encode(tx);
+    console.log("STx", serializedTransaction);
+
+    //const base64TxAddr = getBase64EncodedWireTransaction(tx);
     const txPurpose =
-      `Transfer ${lamportCount} lamports to ${recipientAcct}`;
+      `Transfer ${lamportCount} lamports to ${recipientAccount}`;
 
     return Response.json({
-//      transaction: base64TxAddr,
+      transaction: base64TxAddr,
       message: txPurpose
     });
   } catch (error) {

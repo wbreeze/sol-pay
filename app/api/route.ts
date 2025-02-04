f091ed84259c81d2e877a3f28be045273d943fac
Douglas Lovell <doug@wbreeze.com>
2025-02-04 19:26:18 -0300

    Return configured recipient account in api list

--- app/api/route.ts
+++ app/api/route.ts
@@ -1,9 +1,13 @@
 export async function GET(request: Request) {
-  return Response.json([
+  return Response.json({
+    api:[
       { methods: [ 'get' ], path: '/api/' },
       { methods: [ 'get' ], path: '/api/tx/' },
       { methods: [ 'post' ], path: '/api/tx/' },
       { methods: [ 'get' ], path: '/photos' },
-  ]);
+      { methods: [ 'get' ], path: '/pos' },
+    ],
+    account: process.env.TX_RECIPIENT_ACCOUNT
+  });
 }
 
